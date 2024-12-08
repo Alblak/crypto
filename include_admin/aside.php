a2e9a74dbfc4a94b1dfe650aaafc62f4a786f6b0
Alblak <albertkambale16@gmail.com>
2024-12-09 00:16:23 +0200

    add others files

---
+++ include_admin/aside.php
@@ -0,0 +1,35 @@
+<ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon">
+          <img src="img/logo/logo2.png">
+        </div>
+        <div class="sidebar-brand-text mx-3">Admin</div>
+      </a>
+      <hr class="sidebar-divider my-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="../views/cesar.php">
+        <i class="fas fa-fw fa-columns"></i>
+          <span>cesar</span></a>
+      </li>
+      <li class="nav-item active">
+      <a class="nav-link" href="../views/affine.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Affine</span></a>
+      </li>
+      <li class="nav-item active">
+      <a class="nav-link" href="../views/vigenere.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Vigenere</span></a>
+      </li>
+
+      <li class="nav-item active">
+      <a class="nav-link" href="../views/hill.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>hill</span></a>
+      </li>
+      <hr class="sidebar-divider">
+     
+   
+     
+      <div class="version" id="version-ruangadmin"></div>
+    </ul>
---
+++ include_admin/lien.php
@@ -0,0 +1,5 @@
+<link href="../assets/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="../assets/admin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="../assets/admin/css/ruang-admin.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="../assets/font/fonts/bootstrap-icons.min.css">
