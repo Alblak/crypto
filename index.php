fb4d3fc0cfad4fc823d286c12c88d7d9e8664152
Alblak <albertkambale16@gmail.com>
2024-12-09 00:13:28 +0200

    add index

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="img/logo/logo.png" rel="icon">
+  <title>accueil</title>
+  <link href="assets/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="assets/admin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="assets/admin/css/ruang-admin.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="assets/font/fonts/bootstrap-icons.min.css">
+  
+</head>
+
+<body id="page-top">
+  <div id="wrapper">
+    <!-- Sidebar -->
+   
+    <!-- Sidebar -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <!-- TopBar -->
+     
+        <!-- Topbar -->
+
+        <!-- Container Fluid-->
+        <div class="container-fluid" id="container-wrapper">
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+           
+           
+          </div>
+
+       
+          <div class="row">
+            <div class="col-12">
+                <center>Pour chiffrer et dechiffrer les messages </center>
+            </div>
+            <div class="col-12">
+                <center><a class="btn btn-danger" href="views/admin.php">CLiquer ici </a></center>
+            </div>
+   
+                  
+   
+       
+      </div>
+  
+    </div>
+  </div>
+
+  <!-- Scroll to top -->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+ <?php  include_once('include_admin/script.php'); ?>
+
+  
+</body>
+
+</html>
